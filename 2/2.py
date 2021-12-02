e6615b03b41a3dcd4ca331c65b0daa4a90fa9303
gina.diez <georgina.diez@attackiq.com>
2021-12-02 23:06:36 +0100

    Add second part

--- 2/2.py
+++ 2/2.py
@@ -13,8 +13,31 @@ def get_position(input_file):
     return position * depth
 
 
+def get_position_by_aim(input_file):
+    position = 0
+    depth = 0
+    aim = 0
+    for step in input_file:
+        movement, amount = step.split(' ')
+        if movement == "forward":
+            position += int(amount)
+            if aim > 0:
+                depth += int(amount) * aim
+        elif movement == "down":
+            aim += int(amount)
+        elif movement == "up":
+            aim -= int(amount)
+
+    return position * depth
+
+
 if __name__ == "__main__":
     with open("input.txt", "r") as input_file:
         input_file_lines = input_file.read().splitlines()
         result = get_position(input_file_lines)
-        print(result)
+        print(f'First part answer: {result}')
+
+    with open("input.txt", "r") as input_file:
+        input_file_lines = input_file.read().splitlines()
+        result = get_position_by_aim(input_file_lines)
+        print(f'Second part answer: {result}')
