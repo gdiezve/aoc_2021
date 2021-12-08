20333d4e9508c2854b161683481db494a440b675
gina.diez <georgina.diez@attackiq.com>
2021-12-08 11:58:25 +0100

    Finish part 2

--- 3/3.py
+++ 3/3.py
@@ -4,10 +4,8 @@ def get_gamma_epsilon_rates(input_file):
         if not sum:
             sum = [bit for bit in line]
         else:
-            i = 0
-            while i < len(line):
+            for i in range(len(line)):
                 sum[i] = str(int(sum[i]) + int(line[i]))
-                i += 1
     most_common = ''
     less_common = ''
     for pos in sum:
@@ -20,44 +18,54 @@ def get_gamma_epsilon_rates(input_file):
     return int(most_common, 2) * int(less_common, 2)
 
 
-def get_oxygen_co2_rates(input_file):
-    oxygen_list = [line for line in input_file]
-    sum = get_most_common_bit(input_file)
-    for pos in sum:
-        print(sum)
-        print(pos)
-        print(len(oxygen_list))
-        if len(oxygen_list) == 1:
-            break
-        if int(sum[sum.index(pos)]) > len(oxygen_list) / 2:  # most common 1
-            oxygen_list = [line for line in oxygen_list if line[sum.index(pos)] == '1']
-        elif int(sum[sum.index(pos)]) < len(oxygen_list) / 2:  # most common 0
-            oxygen_list = [line for line in oxygen_list if line[sum.index(pos)] == '0']
-        elif int(sum[sum.index(pos)]) == len(oxygen_list) / 2:
-            oxygen_list = [line for line in oxygen_list if line[sum.index(pos)] == '1']
-        sum = get_most_common_bit(oxygen_list)
-        print(sum)
-
-    print(f'oxygen {oxygen_list}')
-
-
-def get_most_common_bit(input_file):
-    sum = []
-    for line in input_file:
-        if not sum:
-            sum = [bit for bit in line]
+def get_oxygen_generator_rating(input_file):
+    bit_criteria = get_bit_criteria(input_file)
+    input = input_file
+    i = 0
+    while i < len(input[0]) and len(input) > 1:
+        if float(bit_criteria[i]) >= len(input)/2:
+            input = [line for line in input if line[i] == '1']
         else:
-            i = 0
-            while i < len(line):
-                sum[i] = str(int(sum[i]) + int(line[i]))
-                i += 1
-    return sum
+            input = [line for line in input if line[i] == '0']
+        bit_criteria = get_bit_criteria(input)
+        i += 1
+    return input
+
+
+def get_co2_scrubber_rating(input_file):
+    bit_criteria = get_bit_criteria(input_file)
+    input = input_file
+    i = 0
+    while i < len(input[0]) and len(input) > 1:
+        if float(bit_criteria[i]) >= len(input)/2:
+            input = [line for line in input if line[i] == '0']
+        else:
+            input = [line for line in input if line[i] == '1']
+        bit_criteria = get_bit_criteria(input)
+        i += 1
+    return input
+
+
+def get_bit_criteria(input):
+    bit_criteria = []
+    for line in input:
+        if not bit_criteria:
+            bit_criteria = [bit for bit in line]
+        else:
+            for i in range(len(line)):
+                bit_criteria[i] = str(int(bit_criteria[i]) + int(line[i]))
+    return bit_criteria
+
 
 if __name__ == "__main__":
     with open("input.txt", "r") as input_file:
         input_file_lines = input_file.read().splitlines()
-        get_gamma_epsilon_rates(input_file_lines)
+        result = get_gamma_epsilon_rates(input_file_lines)
+        print(f'First part answer: {result}')
 
     with open("input.txt", "r") as input_file:
         input_file_lines = input_file.read().splitlines()
-        get_oxygen_co2_rates(input_file_lines)
+        oxygen = get_oxygen_generator_rating(input_file_lines)
+        co2 = get_co2_scrubber_rating(input_file_lines)
+        result = int(oxygen[0], 2) * int(co2[0], 2)
+        print(f'Second part answer: {result}')
